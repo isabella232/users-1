7e8f7578515d90522110062c7646194ebbd30a9c
ngervais <ngervais>
2008-09-23 16:34:32 +0000

    New Dual Implementation

--- functions.js
+++ functions.js
@@ -1,8 +1,14 @@
 function flipAnimation () {
-	var helpFade = new YAHOO.util.Anim("help", {	opacity:{to:0} }, 1, YAHOO.util.Easing.easeOut );
-	var helpShow = new YAHOO.util.Anim("help", {	opacity:{to:1} }, 1, YAHOO.util.Easing.easeIn );
-	var categoriesFade = new YAHOO.util.Anim("categories", {	opacity:{to:0} }, 1, YAHOO.util.Easing.easeOut );
-	var categoriesShow = new YAHOO.util.Anim("categories", {	opacity:{to:1} }, 1, YAHOO.util.Easing.easeIn );
+	//Top Section
+	var helpTopFade = new YAHOO.util.Anim("help", {	opacity:{to:0} }, 1, YAHOO.util.Easing.easeOut );
+	var helpTopShow = new YAHOO.util.Anim("help", {	opacity:{to:1} }, 1, YAHOO.util.Easing.easeIn );
+	var categoriesTopFade = new YAHOO.util.Anim("categories", {	opacity:{to:0} }, 1, YAHOO.util.Easing.easeOut );
+	var categoriesTopShow = new YAHOO.util.Anim("categories", {	opacity:{to:1} }, 1, YAHOO.util.Easing.easeIn );
+	//List Section
+	var helpListFade = new YAHOO.util.Anim("helpList", {	opacity:{to:0} }, 1, YAHOO.util.Easing.easeOut );
+	var helpListShow = new YAHOO.util.Anim("helpList", {	opacity:{to:1} }, 1, YAHOO.util.Easing.easeIn );
+	var categoriesListFade = new YAHOO.util.Anim("categoriesList", {	opacity:{to:0} }, 1, YAHOO.util.Easing.easeOut );
+	var categoriesListShow = new YAHOO.util.Anim("categoriesList", {	opacity:{to:1} }, 1, YAHOO.util.Easing.easeIn );
 	
 	function show (e) { 
 		var el = document.getElementById(e);
@@ -11,28 +17,55 @@ function flipAnimation () {
 	
 	function hide (e) { var el = document.getElementById(e); el.className = "invisible"; }
 	
-	var helpStart = function (type, args) {
-		var toggleLink = document.getElementById('toggleLink');
-		var toggleText = document.getElementById('toggleText');
+	//Top Help Functions
+	var helpFadeStart = function (type, args) {
 		show('categories');			
-
 	}
+	var helpShowStart = function (type, args) {
+		var help = document.getElementById('help');
+		help.style.opacity = 0;
+	}
+	var helpFadeComplete = function (type, args) { hide('help'); hide('categoriesList'); show('helpList'); helpListShow.animate(); }
+	helpTopFade.onStart.subscribe(helpFadeStart);
+	helpTopFade.onComplete.subscribe(helpFadeComplete);
+	helpTopShow.onStart.subscribe(helpShowStart);
 	
-	var helpComplete = function (type, args) { hide('help'); }
-	
-	var categoriesStart = function (type, args) {
-		var toggleLink = document.getElementById('toggleLink');
-		var toggleText = document.getElementById('toggleText');
+	//Top Categories Functions
+	var categoriesFadeStart = function (type, args) {
 		show('help');
 	}
-	
-	var categoriesComplete = function (type, args) { hide('categories'); }
+	var categoriesShowStart = function (type, args) {
+		var categories = document.getElementById('categories');
+		categories.style.opacity = 0;
+	}
+	var categoriesFadeComplete = function (type, args) { hide('categories');}
+	categoriesTopFade.onStart.subscribe(categoriesFadeStart);
+	categoriesTopFade.onComplete.subscribe(categoriesFadeComplete);
+	categoriesTopShow.onStart.subscribe(categoriesShowStart)
 
-	helpFade.onStart.subscribe(helpStart);
-	helpFade.onComplete.subscribe(helpComplete);
+
+
+
+
+
+	//List Help Functions
+	var helpListShowStart = function (type, args) { 
+		var helpList = document.getElementById('helpList');
+		helpList.style.opacity = 0;
+	}
+	var helpListFadeComplete = function (type, args) { hide('helpList'); show('categoriesList'); categoriesListShow.animate(); }
+	helpListShow.onStart.subscribe(helpListShowStart);
+	helpListFade.onComplete.subscribe(helpListFadeComplete);
+
+	//List Categories Functions
+	var categoriesListShowStart = function (type, args) { 
+		var categoriesList = document.getElementById('categoriesList');
+		categoriesList.style.opacity = 0;
+	}
+	var categoriesListFadeComplete = function (type, args) { hide('categoriesList'); show('helpList'); helpListShow.animate(); }
+	categoriesListShow.onStart.subscribe(categoriesListShowStart);
+	categoriesListFade.onComplete.subscribe(categoriesListFadeComplete);
 	
-	categoriesFade.onStart.subscribe(categoriesStart);
-	categoriesFade.onComplete.subscribe(categoriesComplete);
 	var exdate = new Date();
 	exdate.setDate(exdate.getDate()+7);
 	var checkState = document.getElementById('help');
@@ -42,11 +75,15 @@ function flipAnimation () {
 		if (YAHOO.env.ua.ie > 0)
 		{
 			show('categories');
-			hide('help');		
-		}
+			hide('help');
+			hide('categoriesList');
+			show('helpList');
+		}		
 		else {
-			helpFade.animate();
-			categoriesShow.animate();
+			categoriesListFade.animate();
+			helpTopFade.animate();
+			categoriesTopShow.animate();
+			
 		}
 	}
 	else
@@ -55,11 +92,15 @@ function flipAnimation () {
 		if (YAHOO.env.ua.ie > 0)
 		{
 			show('help');
-			hide('categories');		
+			hide('categories');	
+			hide('helpList');
+			show('categoriesList');	
 		}
 		else {
-			categoriesFade.animate();
-			helpShow.animate();
+			helpListFade.animate();
+			categoriesTopFade.animate();
+			helpTopShow.animate();
+			
 		}
 	}	
 }
--- index.php
+++ index.php
@@ -35,9 +35,9 @@
 	//$whatsnew = rss_to_html('whatsnew');			
 	# Paste your HTML content between the EOHTML markers!	
 	require_once ($root . "/community/news/scripts/newsfeeds.php");
-	$communitynews = @ rss_to_news_html($root . "/community/news/2005inthenewsarchive.rss", "news", 4);
-	$epicPlugins = @ rss_to_news_html("/home/data/httpd/writable/community/pluginfeed.rss", "", 5);
-	$eclipseLiveFeed = @ rss_to_news_html("/home/data/httpd/writable/community/eclipselive.rss", "", 4);
+	$communitynews = @ rss_to_news_html($root . "/community/news/2005inthenewsarchive.rss", "news", 6);
+	$epicPlugins = @ rss_to_news_html("/home/data/httpd/writable/community/pluginfeed.rss", "", 7);
+	$eclipseLiveFeed = @ rss_to_news_html("/home/data/httpd/writable/community/eclipselive.rss", "", 6);
 	
 	$cookie = $_COOKIE['userSwap'];
 	if (!isset($cookie))
@@ -51,105 +51,149 @@
 <script type="text/javascript" src="functions.js"></script>
 <link rel="stylesheet" type="text/css" href="users.css" media="screen" />
 <div id="widecontainer">
-	<div id="constant">
-		<div id="users" class="floatingIcon">
-			<img src="images/users.png"><br/>
-			<p class="heading">Users</p>
-		</div>
-		<div id="toggle" class="floatingIcon swap">
-			<a onclick="flipAnimation();"><img src="images/swap.png"></a><br/>	
-			<a class="heading" id="toggleLink" onclick="flipAnimation();">Switch View</a>
-		</div>	
-	</div>		
-	<div id="help" <? if ($cookie !="help") echo "class=\"invisible\"";?>>
-		<div id="events" class="floatingIcon">
-			<a href="events/"><img src="http://dev.eclipse.org/huge_icons/apps/office-calendar.png"></a><br/>
-			<a class="heading" href="events/">Events</a>
-			<p class="subText">Check out a conference<br/> or gathering near you.</p>
-		</div>
-		<div id="portals" class="floatingIcon">
-			<a href="portals.php"><img src="http://dev.eclipse.org/huge_icons/apps/preferences-system-network-proxy.png"></a><br/>
-			<a class="heading" href="/community/portals.php">Portals</a>
-			<p class="subText">Community sites and blogs<br/> around the web.</p>
-		</div>
-		<div id="training" class="floatingIcon">
-			<img src="http://dev.eclipse.org/huge_icons/mimetypes/x-office-presentation.png"><br/>
-			<a class="heading">Training</a>
-			<p class="subText"><a href="/community/books/">Books</a>, <a href="/community/training.php">Online Courses</a>,<br/> <a href="http://live.eclipse.org">Webinars</a> and <a href="/resources/?sort=date&category=Tutorial">Tutorials</a>.</p>
-		</div>
-		<!-- <div id="consulting" class="floatingIcon">
-			<a href="http://www.eclipseplugincentral.com/Services-index-req-viewcatlink-cid-1.html"><img src="http://dev.eclipse.org/huge_icons/categories/applications-development.png"></a><br/>
-			<a class="heading">Consulting &amp; Services</a>
-			<p class="subText">Consulting, development, and training services.</p>
-		</div> -->
-		<div id="resources" class="floatingIcon">
-			<a href="/resources/"><img src="http://dev.eclipse.org/huge_icons/mimetypes/x-office-book.png"></a><br/>
-			<a class="heading" href="/resources/">Resources</a>
-			<p class="subText">Find Eclipse Articles<br/> and Resources.</p>
-		</div>
-		<!-- <div id="rcp" class="floatingIcon">
-			<a href="rcp.php"><img src="http://dev.eclipse.org/huge_icons/apps/preferences-system-windows.png"></a><br/>
-			<a class="heading">RCP Catalog</a>
-			<p class="subText">Open Source, and Commercial Rich Client Platform applications.</p>
-		</div> -->
-		<div id="project" class="floatingIcon">
-			<a href="/projects/listofprojects.php"><img src="http://dev.eclipse.org/huge_icons/actions/mail-forward.png"></a><br/>
-			<a class="heading" href="/projects/listofprojects.php">Projects</a>
-		</div>
-		<!-- <div id="plugins" class="floatingIcon">
-			<a href="http://www.eclipseplugincentral.com/"><img src="http://dev.eclipse.org/huge_icons/apps/preferences-desktop-remote-desktop.png"></a><br/>
-			<a class="heading">Plugins</a>
-			<p class="subText">Check out the many<br/>Eclipse-based plugins.</p>
-		</div> -->
-		<div id="documentation" class="floatingIcon">
-			<a href="http://help.eclipse.org/"><img src="http://dev.eclipse.org/huge_icons/apps/help-browser.png"></a><br/>
-			<a class="heading" href="http://help.eclipse.org/">Documentation</a>
-		</div>
-	</div>
-	<div id="categories" <? if ($cookie !="categories") echo "class=\"invisible\"";?>>
-		<div id="enterprise" class="floatingIcon">
-			<a href="/home/categories/enterprise.php"><img src="http://dev.eclipse.org/huge_icons/places/network-server.png"></a><br/>
-			<a class="heading" href="/home/categories/enterprise.php">Enterprise Development</a>
-		</div>
-		<div id="embedded" class="floatingIcon">
-			<a href="/home/categories/embedded.php"><img src="http://dev.eclipse.org/custom_icons/embedded64x64.png"></a><br/>
-			<a class="heading" href="/home/categories/embedded.php">Embedded + Device Development</a>
-		</div>
-		<div id="equinox" class="floatingIcon">
-			<a href="/equinox-portal/"><img height="64" width="64" src="images/equinox.png"></a><br/>
-			<a class="heading" href="/equinox-portal/">RCP + Equinox Runtimes</a>
-		</div>
-		<div id="frameworks" class="floatingIcon">
-			<a href="/home/categories/frameworks.php"><img src="http://dev.eclipse.org/huge_icons/categories/applications-system.png"></a><br/>
-			<a class="heading" href="/home/categories/frameworks.php">Application Frameworks</a>
-		</div>
-		<div id="languages" class="floatingIcon">
-			<a href="/home/categories/languages.php"><img src="http://dev.eclipse.org/huge_icons/apps/preferences-system-session.png"></a><br/>
-			<a class="heading" href="/home/categories/languages.php">Language IDE</a>
-		</div>								
-	</div>	
-	<div id="bottomContent">
-		<div id="left"> 
-			<div class="headerBox">
-				<img class="headerBoxIcons" src="http://dev.eclipse.org/huge_icons/devices/network-wireless.png">
-				<a class="header" href="http://live.eclipse.org/">Eclipse Live</a> <a class="rssIcon" href="http://feeds.feedburner.com/eclipselive"><img align="bottom" src="http://www.eclipse.org/images/rss2.gif"></a>				
-				<p><?=$eclipseLiveFeed;?></p>
+	<div class="topContent">
+		<div id="constant">
+			<div id="users" class="floatingIcon">
+				<img src="images/users.png"><br/>
+				<p class="heading">Users</p>
 			</div>
-		</div>
-		<div id="middle">
-			<div class="headerBox">
-				<img class="headerBoxIcons" src="http://dev.eclipse.org/huge_icons/apps/preferences-desktop-remote-desktop.png">
-				<a class="header" href="http://www.eclipseplugincentral.com/">Eclipse Plugin Central</a> <a class="rssIcon" href="http://www.eclipseplugincentral.com/pluginfeed.rss"><img align="bottom" src="http://www.eclipse.org/images/rss2.gif"></a>
-				<p><?=$epicPlugins;?></p>
-			</div>
+			<div id="toggle" class="floatingIcon swap">
+				<a onclick="flipAnimation();"><img src="images/swap.png"></a><br/>	
+				<a class="heading" id="toggleLink" onclick="flipAnimation();">Switch View</a>
+			</div>			
 		</div>		
-		<div id="right">
+		<div id="help" <? if ($cookie !="help") echo "class=\"invisible\"";?>>
+			<div id="events" class="floatingIcon">
+				<a href="events/"><img src="http://dev.eclipse.org/huge_icons/apps/office-calendar.png"></a><br/>
+				<a class="heading" href="events/">Events</a>
+				<p class="subText">Check out a conference<br/> or gathering near you.</p>
+			</div>
+			<div id="portals" class="floatingIcon">
+				<a href="portals.php"><img src="http://dev.eclipse.org/huge_icons/apps/preferences-system-network-proxy.png"></a><br/>
+				<a class="heading" href="/community/portals.php">Portals</a>
+				<p class="subText">Community sites and blogs<br/> around the web.</p>
+			</div>
+			<div id="training" class="floatingIcon">
+				<a href=""><img src="http://dev.eclipse.org/huge_icons/mimetypes/x-office-presentation.png"></a><br/>
+				<a class="heading">Training</a>
+				<p class="subText"><a href="/community/books/">Books</a>, <a href="/community/training.php">Online Courses</a>, <a href="http://live.eclipse.org">Webinars</a> and <a href="/resources/?sort=date&category=Tutorial">Tutorials</a>.</p>
+			</div>
+			<div id="resources" class="floatingIcon">
+				<a href="/resources/"><img src="http://dev.eclipse.org/huge_icons/mimetypes/x-office-book.png"></a><br/>
+				<a class="heading" href="/resources/">Resources</a>
+				<p class="subText">Find Eclipse Articles<br/> and Resources.</p>
+			</div>
+			<div id="project" class="floatingIcon">
+				<a href="/projects/listofprojects.php"><img src="http://dev.eclipse.org/huge_icons/actions/mail-forward.png"></a><br/>
+				<a class="heading" href="/projects/listofprojects.php">Projects</a>
+			</div>
+			<div id="documentation" class="floatingIcon">
+				<a href="http://help.eclipse.org/"><img src="http://dev.eclipse.org/huge_icons/apps/help-browser.png"></a><br/>
+				<a class="heading" href="http://help.eclipse.org/">Documentation</a>
+			</div>		
+		</div>
+		<div id="categories" <? if ($cookie !="categories") echo "class=\"invisible\"";?>>
+			<div id="enterprise" class="floatingIcon">
+				<a href="/home/categories/enterprise.php"><img src="http://dev.eclipse.org/huge_icons/places/network-server.png"></a><br/>
+				<a class="heading" href="/home/categories/enterprise.php">Enterprise Development</a>
+			</div>
+			<div id="embedded" class="floatingIcon">
+				<a href="/home/categories/embedded.php"><img src="http://dev.eclipse.org/custom_icons/embedded64x64.png"></a><br/>
+				<a class="heading" href="/home/categories/embedded.php">Embedded + Device Development</a>
+			</div>
+			<div id="equinox" class="floatingIcon">
+				<a href="/equinox-portal/"><img height="64" width="64" src="images/equinox.png"></a><br/>
+				<a class="heading" href="/equinox-portal/">RCP + Equinox Runtimes</a>
+			</div>
+			<div id="frameworks" class="floatingIcon">
+				<a href="/home/categories/frameworks.php"><img src="http://dev.eclipse.org/huge_icons/categories/applications-system.png"></a><br/>
+				<a class="heading" href="/home/categories/frameworks.php">Application Frameworks</a>
+			</div>
+			<div id="languages" class="floatingIcon">
+				<a href="/home/categories/languages.php"><img src="http://dev.eclipse.org/huge_icons/apps/preferences-system-session.png"></a><br/>
+				<a class="heading" href="/home/categories/languages.php">Language IDE</a>
+			</div>								
+		</div>
+	</div>		
+	<div class="bottomContent">
+		<div class="left"> 
 			<div class="headerBox">
 				<img class="headerBoxIcons" src="http://dev.eclipse.org/huge_icons/apps/internet-news-reader.png">
 				<a class="header" href="news/eclipseinthenews.php">Community News</a> <a class="rssIcon" href="/home/eclipseinthenews.rss"><img align="bottom" src="http://www.eclipse.org/images/rss2.gif"></a>				
 				<p><?=$communitynews;?></p>
 			</div>
 		</div>
+		<div class="right">
+				<div class="headerBox">	
+					<div id="categoriesList" <? if ($cookie =="categories") echo "class=\"invisible\"";?>>
+						<a class="header">Pillars of Eclipse</a><br/><br/>
+						<div id="enterprise" class="pillar">
+							<a href="/home/categories/enterprise.php"><img src="http://dev.eclipse.org/large_icons/places/network-server.png"></a>
+							<a class="heading" href="/home/categories/enterprise.php">Enterprise Development</a>
+						</div>
+						<div id="embedded" class="pillar">
+							<a href="/home/categories/embedded.php"><img width="32" height="32" src="http://dev.eclipse.org/custom_icons/embedded64x64.png"></a>
+							<a class="heading" href="/home/categories/embedded.php">Embedded + Device Development</a>
+						</div>
+						<div id="equinox" class="pillar">
+							<a href="/equinox-portal/"><img height="32" width="32" src="images/equinox.png"></a>
+							<a class="heading" href="/equinox-portal/">RCP + Equinox Runtimes</a>
+						</div>
+						<div id="frameworks" class="pillar">
+							<a href="/home/categories/frameworks.php"><img src="http://dev.eclipse.org/large_icons/categories/applications-system.png"></a>
+							<a class="heading" href="/home/categories/frameworks.php">Application Frameworks</a>
+						</div>
+						<div id="languages" class="pillar">
+							<a href="/home/categories/languages.php"><img src="http://dev.eclipse.org/large_icons/apps/preferences-system-session.png"></a>
+							<a class="heading" href="/home/categories/languages.php">Language IDE</a>
+						</div>
+					</div>
+					<div id="helpList" <? if ($cookie =="help") echo "class=\"invisible\"";?>>
+						<a class="header">Help Topics</a><br/><br/>
+						<div id="project" class="pillar">
+							<a href="/projects/listofprojects.php"><img src="http://dev.eclipse.org/large_icons/actions/mail-forward.png"></a>
+							<a class="heading" href="/projects/listofprojects.php">Projects</a>
+						</div>						
+						<div id="resources" class="pillar">
+							<a href="/resources/"><img height="32" width="32" src="http://dev.eclipse.org/huge_icons/mimetypes/x-office-book.png"></a>
+							<a class="heading" href="/resources/">Resources</a>
+						</div>
+						<div id="documentation" class="pillar">
+							<a href="http://help.eclipse.org/"><img src="http://dev.eclipse.org/large_icons/apps/help-browser.png"></a>
+							<a class="heading" href="http://help.eclipse.org/">Documentation</a>
+						</div>					
+						<div id="training" class="pillar">
+							<a href=""><img src="http://dev.eclipse.org/large_icons/mimetypes/x-office-presentation.png"></a>
+							<a class="heading">Training</a>
+						</div>
+						<div id="events" class="pillar">
+							<a href="events/"><img src="http://dev.eclipse.org/large_icons/apps/office-calendar.png"></a>
+							<a class="heading" href="events/">Events</a>
+						</div>
+						<div id="portals" class="pillar">
+							<a href="portals.php"><img src="http://dev.eclipse.org/large_icons/apps/preferences-system-network-proxy.png"></a>
+							<a class="heading" href="/community/portals.php">Portals</a>
+						</div>
+					</div>								
+				</div>	
+		</div>
+	</div>
+	<div class="bottomContent">
+		<div class="left">
+			<div class="headerBox">
+				<img class="headerBoxIcons" src="http://dev.eclipse.org/huge_icons/devices/network-wireless.png">
+				<a class="header" href="http://live.eclipse.org/">Eclipse Live</a> <a class="rssIcon" href="http://feeds.feedburner.com/eclipselive"><img align="bottom" src="http://www.eclipse.org/images/rss2.gif"></a>				
+				<p><?=$eclipseLiveFeed;?></p>
+			</div>
+		</div>
+		<div class="right">
+			<div class="headerBox">
+				<img class="headerBoxIcons" src="http://dev.eclipse.org/huge_icons/apps/preferences-desktop-remote-desktop.png">
+				<a class="header" href="http://www.eclipseplugincentral.com/">Eclipse Plugin Central</a> <a class="rssIcon" href="http://www.eclipseplugincentral.com/pluginfeed.rss"><img align="bottom" src="http://www.eclipse.org/images/rss2.gif"></a>
+				<p><?=$epicPlugins;?></p>
+			</div>
+		</div>		
+
 	</div>
 </div>
 	<?
