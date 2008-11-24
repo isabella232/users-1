5352b265fe7e036bd9ac1e4214992c9d338cad2d
ngervais <ngervais>
2008-11-24 17:25:24 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -204,9 +204,6 @@
 				<p><?=$epicPlugins;?></p>
 			</div>
 		</div>		
-		<div class="ads" align="center">
-			<a href="http://eclipse.org/go/ESE_Banner@USERS"><img border="0" src="http://www.eclipsesummit.org/summiteurope2008/image/friends/480x60.jpg" width="480" height="60" alt="Eclipse Summit Europe 2008"/></a><br/><br/>
-		</div>
 	</div>
 </div>
 	<?
