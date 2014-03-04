c3a9f2ccea76ba1892788fdf225e59c679076cfe
Christopher Guindon <chris.guindon@eclipse.org>
2014-03-04 17:02:01 -0500

    update content of user page
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

---
+++ content/en_index.php
@@ -0,0 +1,48 @@
+<div class="row">
+
+	<div class="col-md-6">
+		<h1>Getting Started</h1>
+		<p>You can download the standard version of Eclipse that contains the basic bits
+		for any Java developer to start coding in Java.</p>
+		<p><a href="/downloads/" class="btn btn-warning">Download</a></p>
+		<p> Eclipse also has pre-defined packages based on the type of development you want to do with Eclipse.
+		Check out what packages we have to offer.</p>
+		<p><a href="/downloads/" class="btn btn-warning">Download Packages </a></p>
+	</div>
+
+	<div class="col-md-6">
+		<h1>Extend Eclipse</h1>
+		<p>Eclipse Marketplace is a great source of plug-ins and products that you can add to your
+		Eclipse installation. Browser the online catalog or even better start-up the Eclipse
+		Marketplace Client from within Eclipse. Look under the Help menu.</p>
+		<p>Some of the more popular plugins for Eclipse include: </p>
+		<ul>
+		  <li>Subversive - SVN Team Provider</li>
+		  <li>Subclipse</li>
+		  <li>Eclipse Color Theme </li>
+		  <li>Maven Integration for Eclipse (Juno and newer)</li>
+		</ul>
+	</div>
+
+	<div class="col-md-6">
+	  <h1>Documentation</h1>
+	  <p>Some popular getting started documents for someone new to Eclipse: </p>
+	  <ul>
+	  	<li>Getting Started with the Eclipse Workbench</li>
+    	<li>Getting Started with Java development</li>
+    	<li>All online Documentation</li>
+    </ul>
+
+	</div>
+
+	<div class="col-md-6">
+		<h1>Getting Help</h1>
+		<ul>
+			<li>There are many online source of help in the Eclipse community. First thing to do is create an account so you can use them.</li>
+    	<li>Our forums are great places to ask questions, especially the newcomer forum</li>
+    	<li>Open bugs and feature requests at bugzilla</li>
+    	<li>IRC channels are active for some projects</li>
+   		</li>Project mailing list are good source of what is going on in the project</li>
+	</div>
+
+</div>
--- index.php
+++ index.php
@@ -15,87 +15,14 @@
 	$pageTitle 		= "Getting Started with Eclipse";
 	$pageKeywords	= "eclipse resources, courses, books, events, plug-ins, Getting Started with Eclipse, help eclipse, eclipse documentation links, Get Involved with Eclipse, support eclipse";
 	$pageAuthor		= "Christopher Guindon";
-	$root = $_SERVER['DOCUMENT_ROOT'];
+
+	// Place your html content in a file called content/en_pagename.php
 	ob_start();
-	?>
-
- <div id="widecontainer">
-	<div class="topContent">
-		<script type="text/javascript">
-		/* <![CDATA[ */
-			document.write('<div class=\"addthis_toolbox addthis_default_style round\"><a class=\"addthis_button_facebook_like\" fb:like:layout=\"button_count\"><\/a><a class=\"addthis_button_tweet\"><\/a><a class=\"addthis_button_google_plusone\" g:plusone:size=\"medium\"><\/a><a class=\"addthis_counter addthis_pill_style\"><\/a><\/div>');
-		/* ]]> */
-		</script>
-		<div class="box_user box_right round shadow" id="box_user_newto">
-			<h2>Getting Started with Eclipse</h2>
-			<div class="box_content">
-				<ul>
-					<li class="box_li_toggle" id="box_li_download"><a href="//www.eclipse.org/downloads/" class="txt_emphasis round">Download an Eclipse package</a> <span class="secondary_links">Learn more about Eclipse <a href="//www.eclipse.org/downloads/compare.php">packages</a>. </span></li>
-					<li class="box_li_toggle"><a href="http://help.eclipse.org">Read the Documentation</a></li>
-					<li class="box_li_toggle">Learn how to <a href="//wiki.eclipse.org/FAQ_How_do_I_upgrade_Eclipse%3F">upgrade Eclipse</a> &amp; <a href="//marketplace.eclipse.org/content/drag-install-now-available-eclipse-marketplace">drag to install on Eclipse Marketplace</a></li>
-					<li class="box_li_toggle" id="box_li_books">More Information: <span class="secondary_links"><a href="//www.eclipse.org/community/eclipse_newsletter/">Eclipse Newsletter</a>, <a href="//www.eclipse.org/resources/?type=book">Books</a>, <a href="//www.eclipse.org/articles/">Articles</a>, <a href="//www.eclipse.org/resources/?sort=date&amp;category=Tutorial">Tutorials</a> &amp; <a href="//www.youtube.com/user/EclipseFdn" target="_blank">YouTube Channel</a> </span></li>
-				</ul>
-				<div class="box_icon"><a href="//www.eclipse.org/downloads/"><span>Download</span><img src="//dev.eclipse.org/huge_icons/actions/document-save.png" alt="Download"/></a></div>
-			</div>
-		</div> <!-- /end #box_user_newto -->
-
-
-		<div class="box_user box_left round shadow" id="box_user_support">
-			<h2>Looking For Help?</h2>
-		<div class="box_content">
-			<ul>
-				<li class="box_li_toggle"><a href="https://dev.eclipse.org/site_login/createaccount.php">Create An Eclipse Account</a><span class="secondary_links"> to access our <a href="//www.eclipse.org/forums">forums</a>, <a href="https://bugs.eclipse.org/bugs">Bugzilla</a>, <a href="//wiki.eclipse.org">Wiki</a> and other Eclipse sites.</span></li>
-				<li class="box_li_toggle" id="box_li_newcomers"><a href="//www.eclipse.org/forums/index.php/f/89/">Ask a question in the Newcomers Forums</a></li>
-				<li class="box_li_toggle">Frequently asked questions: <a href="//www.eclipse.org/home/newcomers.php">Newcomer</a> &amp; <a href="//wiki.eclipse.org/Eclipse_FAQs">Eclipse </a></li>
-
-				<li class="box_li_toggle"><a href="//www.eclipse.org/mail/">Follow project mailing lists</a></li>
-			</ul>
-				<div class="box_icon"><img src="//dev.eclipse.org/huge_icons/apps/help-browser.png" alt="Help"/></div>
-			</div>
-		</div> <!-- /end #box_user_support -->
-
-			<div class="clear"></div>
-		<div class="box_user box_right round shadow" id="box_user_community">
-		<h2>More Eclipse Resources</h2>
-		<div class="box_content">
-			<ul>
-				<li class="box_li_toggle"><a href="//marketplace.eclipse.org/">Discover more Eclipse plugins on Marketplace</a></li>
-				<li class="box_li_toggle" id="box_li_project"><a href="//www.eclipse.org/projects/">Learn more about Eclipse projects</a></li>
-				<li class="box_li_toggle"><a href="//code.google.com/a/eclipselabs.org/hosting/" target="_blank">Other open source projects on Eclipse Labs</a></li>
-				<li class="box_li_toggle"><a href="//www.eclipse.org/community/events/">Eclipse Events</a></li>
-				<li class="box_li_toggle"><a href="//www.eclipse.org/community/portals.php">Developer Portals and User Communities</a></li>
-				<li class="box_li_toggle"><a href="//marketplace.eclipse.org/taxonomy/term/34">Training &amp; Consulting</a> &amp; <a href="//wiki.eclipse.org/Training_Schedule">Training Schedule</a></li>
-			</ul>
-			<div class="box_icon"><img src="//dev.eclipse.org/huge_icons/apps/system-users.png" alt="Users"/></div>
-			</div>
-		</div> <!-- /end #box_user_community -->
-
-
-		<div class="box_user box_left round shadow" id="box_user_involved">
-		<h2>Get Involved</h2>
-		<div class="box_content">
-			<ul>
-				<li class="box_li_toggle"><a href="//wiki.eclipse.org/Development_Resources">Development Resources</a></li>
-				<li class="box_li_toggle"><a href="https://bugs.eclipse.org/bugs/enter_bug.cgi" id="link_bugs">Report Bugs</a></li>
-				<!-- <li class="box_li_toggle"><a href="//wiki.eclipse.org/index.php/CVS_Howto">Source Code</a></li>
-				<li class="box_li_toggle"><a href="//www.eclipse.org/epp/download.php">Nightly Builds</a></li> -->
-				<li class="box_li_toggle"><a href="//www.eclipse.org/donate/">Support Eclipse</a> <span class="secondary_links">Feel free to donate any amount you'd like. Donations of $35 or more will receive special Friends of Eclipse benefits.</span></li>
-				<li class="box_li_toggle"><a href="//wiki.eclipse.org">Update the Wiki</a></li>
-			</ul>
-			<div class="box_icon"><img src="//dev.eclipse.org/huge_icons/devices/network-wired.png" alt="Network wired"/></div>
-		</div>
-		</div> <!-- /end #box_user_involved -->
-
-	<div class="clear"></div>
-
-	</div>		<!-- /end topContent -->
-</div><!-- /end widecontainer -->
-	<?
+	include("content/en_" . $App->getScriptName());
 	$html = ob_get_clean();
-	$html = mb_convert_encoding($html, "HTML-ENTITIES", "auto");
+
 	# Generate the web page
 	$App->Promotion = TRUE;
-	$App->AddExtraHtmlHeader('<script type="text/javascript" src="//s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f0a6769141fbf14"></script>');
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />');
-	$App->generatePage("Nova", $Menu, NULL, $pageAuthor, $pageKeywords, $pageTitle, $html);
+	//$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />');
+	$App->generatePage("solstice", $Menu, NULL, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
