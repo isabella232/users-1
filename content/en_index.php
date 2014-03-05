1625586b5ff7d39aef11020393f23c223606e563
Christopher Guindon <chris.guindon@eclipse.org>
2014-03-04 19:42:54 -0500

    update content

--- content/en_index.php
+++ content/en_index.php
@@ -1,10 +1,11 @@
-<div class="row">
+<div class="row getting-started">
 
 	<div class="col-md-6">
-		<h1>Getting Started</h1>
+		<h1 class="current">Getting Started</h1>
 		<p>You can download the standard version of Eclipse that contains the basic bits
 		for any Java developer to start coding in Java.</p>
 		<p><a href="/downloads/" class="btn btn-warning">Download</a></p>
+		<hr/>
 		<p> Eclipse also has pre-defined packages based on the type of development you want to do with Eclipse.
 		Check out what packages we have to offer.</p>
 		<p><a href="/downloads/" class="btn btn-warning">Download Packages </a></p>
@@ -17,10 +18,10 @@
 		Marketplace Client from within Eclipse. Look under the Help menu.</p>
 		<p>Some of the more popular plugins for Eclipse include: </p>
 		<ul>
-		  <li>Subversive - SVN Team Provider</li>
-		  <li>Subclipse</li>
-		  <li>Eclipse Color Theme </li>
-		  <li>Maven Integration for Eclipse (Juno and newer)</li>
+		  <li><a href="http://marketplace.eclipse.org/content/subversive-svn-team-provider">Subversive - SVN Team Provider</a></li>
+		  <li><a href="http://marketplace.eclipse.org/content/subclipse">Subclipse</a></li>
+		  <li><a href="http://marketplace.eclipse.org/content/eclipse-color-theme">Eclipse Color Them</a></li>
+		  <li><a href="http://marketplace.eclipse.org/content/maven-integration-eclipse-juno-and-newer">Maven Integration for Eclipse (Juno and newer)</a></li>
 		</ul>
 	</div>
 
@@ -38,11 +39,11 @@
 	<div class="col-md-6">
 		<h1>Getting Help</h1>
 		<ul>
-			<li>There are many online source of help in the Eclipse community. First thing to do is create an account so you can use them.</li>
-    	<li>Our forums are great places to ask questions, especially the newcomer forum</li>
+			<li>There are many online source of help in the Eclipse community. First thing to do is <a href="https://dev.eclipse.org/site_login/createaccount.php">create an account</a> so you can use them.</li>
+    	<li>Our <a href="http://eclipse.org/forums/">forums</a> are great places to ask questions, especially the <a href="http://www.eclipse.org/forums/index.php/f/89/">newcomer forum</a></li>
     	<li>Open bugs and feature requests at bugzilla</li>
     	<li>IRC channels are active for some projects</li>
-   		</li>Project mailing list are good source of what is going on in the project</li>
+   		</li>Project <a href="http://www.eclipse.org/mail/">mailing list</a> are good source of what is going on in the project</li>
 	</div>
 
 </div>
--- index.php
+++ index.php
@@ -1,15 +1,22 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-
+<?php
 /*******************************************************************************
- * Copyright (c) 2013 Eclipse Foundation and others.
+ * Copyright (c) 2014 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Christopher Guindon (Eclipse Foundation)- initial API and implementation
+ *    Christopher Guindon (Eclipse Foundation) - Initial implementation
  *******************************************************************************/
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+
+	$App 	= new App();
+	$Nav	= new Nav();
+	$Menu 	= new Menu();
 
 	# Begin: page-specific settings.  Change these.
 	$pageTitle 		= "Getting Started with Eclipse";
