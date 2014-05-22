7e3d5c8b3bdf17a22b090a3e473836927b540233
Christopher Guindon <chris.guindon@eclipse.org>
2014-05-22 11:24:38 -0400

    Update user page with new redesign
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- content/en_index.php
+++ content/en_index.php
@@ -1,50 +1,52 @@
-<div class="row getting-started">
+<div class="timeline">
+<div class="row">
+    <div class="col-md-6 one gs-item">
+      <div class="circle">1</div>
+      <h1 class="fw-600">Getting Started</h1>
+      <p>You can download the standard version of Eclipse that contains the basic bits
+			for any Java developer to start coding in Java.</p>
+			<p><a href="/downloads/" class="btn btn-warning">Download</a></p>
+			<hr/>
+			<p> Eclipse also has pre-defined packages based on the type of development you want to do with Eclipse.
+			Check out what packages we have to offer.</p>
+			<p><a href="/downloads/" class="btn btn-warning">Download Packages </a></p>
+    </div>
+    <div class="col-md-6 two  gs-item">
+      <div class="circle">2</div>
+      <h1>Extend Eclipse</h1>
+      <p>Eclipse Marketplace is a great source of plug-ins and products that you can add to your
+			Eclipse installation. Browser the online catalog or even better start-up the Eclipse
+			Marketplace Client from within Eclipse. Look under the Help menu.</p>
+			<p>Some of the more popular plugins for Eclipse include: </p>
+			<ul>
+			  <li><a href="http://marketplace.eclipse.org/content/subversive-svn-team-provider">Subversive - SVN Team Provider</a></li>
+			  <li><a href="http://marketplace.eclipse.org/content/subclipse">Subclipse</a></li>
+			  <li><a href="http://marketplace.eclipse.org/content/eclipse-color-theme">Eclipse Color Theme</a></li>
+			  <li><a href="http://marketplace.eclipse.org/content/maven-integration-eclipse-juno-and-newer">Maven Integration for Eclipse (Juno and newer)</a></li>
+			</ul>
+    </div>
+    <div class="col-md-6 three gs-item">
+      <div class="circle">3</div>
+      <h1>Documentation</h1>
+      <p>Some popular getting started documents for someone new to Eclipse: </p>
+		  <ul>
+		  	<li>Getting Started with the <a href="http://help.eclipse.org/kepler/nav/0">Eclipse Workbench</a></li>
+	    	<li>Getting Started with <a href="http://help.eclipse.org/kepler/nav/1">Java development</a></li>
+	    	<li>All online <a href="http://www.eclipse.org/documentation/">Documentation</a></li>
+	    </ul>
 
-	<div class="col-md-6">
-		<h1 class="current one"><span>Getting Started</span></h1>
-		<p>You can download the standard version of Eclipse that contains the basic bits
-		for any Java developer to start coding in Java.</p>
-		<p><a href="/downloads/" class="btn btn-warning">Download</a></p>
-		<hr/>
-		<p> Eclipse also has pre-defined packages based on the type of development you want to do with Eclipse.
-		Check out what packages we have to offer.</p>
-		<p><a href="/downloads/" class="btn btn-warning">Download Packages </a></p>
-	</div>
-
-	<div class="col-md-6">
-		<h1 class="two"><span>Extend Eclipse</span></h1>
-		<p>Eclipse Marketplace is a great source of plug-ins and products that you can add to your
-		Eclipse installation. Browser the online catalog or even better start-up the Eclipse
-		Marketplace Client from within Eclipse. Look under the Help menu.</p>
-		<p>Some of the more popular plugins for Eclipse include: </p>
-		<ul>
-		  <li><a href="http://marketplace.eclipse.org/content/subversive-svn-team-provider">Subversive - SVN Team Provider</a></li>
-		  <li><a href="http://marketplace.eclipse.org/content/subclipse">Subclipse</a></li>
-		  <li><a href="http://marketplace.eclipse.org/content/eclipse-color-theme">Eclipse Color Theme</a></li>
-		  <li><a href="http://marketplace.eclipse.org/content/maven-integration-eclipse-juno-and-newer">Maven Integration for Eclipse (Juno and newer)</a></li>
-		</ul>
-	</div>
-
-	<div class="col-md-6">
-	  <h1 class="three"><span>Documentation</span></h1>
-	  <p>Some popular getting started documents for someone new to Eclipse: </p>
-	  <ul>
-	  	<li>Getting Started with the <a href="http://help.eclipse.org/kepler/nav/0">Eclipse Workbench</a></li>
-    	<li>Getting Started with <a href="http://help.eclipse.org/kepler/nav/1">Java development</a></li>
-    	<li>All online <a href="http://www.eclipse.org/documentation/">Documentation</a></li>
-    </ul>
-
-	</div>
-
-	<div class="col-md-6">
-		<h1 class="four"><span>Getting Help</span></h1>
-		<ul>
-			<li>There are many online source of help in the Eclipse community. First thing to do is <a href="https://dev.eclipse.org/site_login/createaccount.php">create an account</a> so you can use them.</li>
-    	<li>Our <a href="http://eclipse.org/forums/">forums</a> are great places to ask questions, especially the <a href="http://www.eclipse.org/forums/index.php/f/89/">newcomer forum</a></li>
-    	<li>Open bugs and feature requests at <a href="https://bugs.eclipse.org/bugs/">bugzilla</a></li>
-    	<li><a href="https://wiki.eclipse.org/IRC">IRC channels</a> are active for some projects</li>
-   		<li>Project <a href="https://dev.eclipse.org/mailman/listinfo">mailing list</a> are good source of what is going on in the project</li>
-   	</ul>
-	</div>
-
+    </div>
+    <div class="col-md-6 four gs-item">
+      <div class="circle">4</div>
+      <h1>Getting Help</h1>
+      <ul>
+				<li>There are many online source of help in the Eclipse community. First thing to do is <a href="https://dev.eclipse.org/site_login/createaccount.php">create an account</a> so you can use them.</li>
+	    	<li>Our <a href="http://eclipse.org/forums/">forums</a> are great places to ask questions, especially the <a href="http://www.eclipse.org/forums/index.php/f/89/">newcomer forum</a></li>
+	    	<li>Open bugs and feature requests at <a href="https://bugs.eclipse.org/bugs/">bugzilla</a></li>
+	    	<li><a href="https://wiki.eclipse.org/IRC">IRC channels</a> are active for some projects</li>
+	   		<li>Project <a href="https://dev.eclipse.org/mailman/listinfo">mailing list</a> are good source of what is going on in the project</li>
+     	</ul>
+    </div>
+    </div>
 </div>
+
