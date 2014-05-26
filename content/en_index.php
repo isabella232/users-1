5b87e36dad3a2a5c3f04deca4c5c99139d0ae329
Christopher Guindon <chris.guindon@eclipse.org>
2014-05-26 16:44:06 -0400

    Bug 435812 - Spelling mistakes on staging.eclipse.org
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- content/en_index.php
+++ content/en_index.php
@@ -28,9 +28,9 @@
       <div class="circle">2</div>
       <h1>Extend Eclipse</h1>
       <p>Eclipse Marketplace is a great source of plug-ins and products that you can add to your
-			Eclipse installation. Browser the online catalog or even better start-up the Eclipse
+			Eclipse installation. Browse the online catalog or even better start-up the Eclipse
 			Marketplace Client from within Eclipse. Look under the Help menu.</p>
-			<p>Some of the more popular plugins for Eclipse include: </p>
+			<p>Some of the more popular plug-ins for Eclipse include: </p>
 			<ul>
 			  <li><a href="http://marketplace.eclipse.org/content/subversive-svn-team-provider">Subversive - SVN Team Provider</a></li>
 			  <li><a href="http://marketplace.eclipse.org/content/subclipse">Subclipse</a></li>
@@ -41,7 +41,7 @@
     <div class="col-md-6 three gs-item">
       <div class="circle">3</div>
       <h1>Documentation</h1>
-      <p>Some popular getting started documents for someone new to Eclipse: </p>
+      <p>These are a few of the popular getting-started documents for someone new to Eclipse: </p>
 		  <ul>
 		  	<li>Getting Started with the <a href="http://help.eclipse.org/kepler/nav/0">Eclipse Workbench</a></li>
 	    	<li>Getting Started with <a href="http://help.eclipse.org/kepler/nav/1">Java development</a></li>
@@ -53,11 +53,11 @@
       <div class="circle">4</div>
       <h1>Getting Help</h1>
       <ul>
-				<li>There are many online source of help in the Eclipse community. First thing to do is <a href="https://dev.eclipse.org/site_login/createaccount.php">create an account</a> so you can use them.</li>
-	    	<li>Our <a href="http://eclipse.org/forums/">forums</a> are great places to ask questions, especially the <a href="http://www.eclipse.org/forums/index.php/f/89/">newcomer forum</a></li>
-	    	<li>Open bugs and feature requests at <a href="https://bugs.eclipse.org/bugs/">bugzilla</a></li>
-	    	<li><a href="https://wiki.eclipse.org/IRC">IRC channels</a> are active for some projects</li>
-	   		<li>Project <a href="https://dev.eclipse.org/mailman/listinfo">mailing list</a> are good source of what is going on in the project</li>
+				<li>There are many online sources of help in the Eclipse community. First thing to do is <a href="https://dev.eclipse.org/site_login/createaccount.php">create an account</a> so you can use them.</li>
+	    	<li>Our <a href="http://eclipse.org/forums/">forums</a> are great places to ask questions, especially the <a href="http://www.eclipse.org/forums/index.php/f/89/">newcomer forum</a>.</li>
+	    	<li>Open bugs and feature requests at <a href="https://bugs.eclipse.org/bugs/">bugzilla</a>.</li>
+	    	<li><a href="https://wiki.eclipse.org/IRC">IRC channels</a> are active for some projects.</li>
+	   		<li>Project <a href="https://dev.eclipse.org/mailman/listinfo">mailing list</a> are good source of what is going on in the project.</li>
      	</ul>
     </div>
     </div>
