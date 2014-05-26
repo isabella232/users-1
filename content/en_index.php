9ea8a0c9e86e96696efe3ca3e4fe7327a3f8824b
Christopher Guindon <chris.guindon@eclipse.org>
2014-05-26 16:42:26 -0400

    adding copyright header
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- content/en_index.php
+++ content/en_index.php
@@ -1,3 +1,16 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2014 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Christopher Guindon (Eclipse Foundation) - Initial implementation
+ *******************************************************************************/
+?>
+
 <div class="timeline">
 <div class="row">
     <div class="col-md-6 one gs-item">
